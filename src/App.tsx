0ec03c9a1172c142efe60eca8e301a0e69f82281
Gabriel Fernando <gf313074@gmail.com>
2025-06-05 21:59:02 -0300

    add routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-export function App() {
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import LayoutMain from "./pages/layout-main";
+import PageComponents from "./pages/page-components";
+import PageHome from "./pages/page-home";
+
+export default function App() {
   return (
-    <>
-      <h1 className=''>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
