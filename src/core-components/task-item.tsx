2b8f0389f6bb41b46315623510099d7f7c94d5b9
Gabriel Fernando <gf313074@gmail.com>
2025-06-10 18:26:54 -0300

    adjust task item with task info

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import { cx } from 'class-variance-authority';
+import React from 'react';
 
-import CheckIcon from "../assets/icons/check.svg?react";
-import PencilIcon from "../assets/icons/pencil.svg?react";
-import TrashIcon from "../assets/icons/trash.svg?react";
-import XIcon from "../assets/icons/x.svg?react";
-import ButtonIcon from "../components/button-icon";
-import Card from "../components/card";
-import InputCheckbox from "../components/input-checkbox";
-import InputText from "../components/input-text";
-import Text from "../components/text";
+import CheckIcon from '../assets/icons/check.svg?react';
+import PencilIcon from '../assets/icons/pencil.svg?react';
+import TrashIcon from '../assets/icons/trash.svg?react';
+import XIcon from '../assets/icons/x.svg?react';
+import ButtonIcon from '../components/button-icon';
+import Card from '../components/card';
+import InputCheckbox from '../components/input-checkbox';
+import InputText from '../components/input-text';
+import Text from '../components/text';
+import { type Task, TaskState } from '../models/task';
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = React.useState(false);
+interface TaskItemProps {
+  task: Task;
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating,
+  );
 
   function handleEditTask() {
     setIsEditing(true);
@@ -22,30 +30,35 @@ export default function TaskItem() {
   }
 
   return (
-    <Card size="md" className="flex items-center gap-4">
+    <Card size='md' className='flex items-center gap-4'>
       {!isEditing ? (
         <>
-          <InputCheckbox />
-          <Text className="flex-1">🛒 Fazer compras da semana</Text>
-          <div className="flex gap-1">
-            <ButtonIcon icon={TrashIcon} variant="tertiary" />
+          <InputCheckbox
+            value={task.concluded?.toString()}
+            checked={task.concluded}
+          />
+          <Text className={cx('flex-1', { 'line-through': task?.concluded })}>
+            {task?.title}
+          </Text>
+          <div className='flex gap-1'>
+            <ButtonIcon icon={TrashIcon} variant='tertiary' />
             <ButtonIcon
               icon={PencilIcon}
-              variant="tertiary"
+              variant='tertiary'
               onClick={handleEditTask}
             />
           </div>
         </>
       ) : (
         <>
-          <InputText className="flex-1" />
-          <div className="flex gap-1">
+          <InputText className='flex-1' />
+          <div className='flex gap-1'>
             <ButtonIcon
               icon={XIcon}
-              variant="secondary"
+              variant='secondary'
               onClick={handleExitEditTask}
             />
-            <ButtonIcon icon={CheckIcon} variant="primary" />
+            <ButtonIcon icon={CheckIcon} variant='primary' />
           </div>
         </>
       )}
