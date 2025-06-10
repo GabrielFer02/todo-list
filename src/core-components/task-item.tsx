33b19c777391babdd60f5fd30b430a257b43419d
Gabriel Fernando <gf313074@gmail.com>
2025-06-10 20:25:37 -0300

    create form to submit task info and validations

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -1,16 +1,16 @@
-import { cx } from 'class-variance-authority';
-import React from 'react';
+import { cx } from "class-variance-authority";
+import React from "react";
 
-import CheckIcon from '../assets/icons/check.svg?react';
-import PencilIcon from '../assets/icons/pencil.svg?react';
-import TrashIcon from '../assets/icons/trash.svg?react';
-import XIcon from '../assets/icons/x.svg?react';
-import ButtonIcon from '../components/button-icon';
-import Card from '../components/card';
-import InputCheckbox from '../components/input-checkbox';
-import InputText from '../components/input-text';
-import Text from '../components/text';
-import { type Task, TaskState } from '../models/task';
+import CheckIcon from "../assets/icons/check.svg?react";
+import PencilIcon from "../assets/icons/pencil.svg?react";
+import TrashIcon from "../assets/icons/trash.svg?react";
+import XIcon from "../assets/icons/x.svg?react";
+import ButtonIcon from "../components/button-icon";
+import Card from "../components/card";
+import InputCheckbox from "../components/input-checkbox";
+import InputText from "../components/input-text";
+import Text from "../components/text";
+import { type Task, TaskState } from "../models/task";
 
 interface TaskItemProps {
   task: Task;
@@ -18,49 +18,67 @@ interface TaskItemProps {
 
 export default function TaskItem({ task }: TaskItemProps) {
   const [isEditing, setIsEditing] = React.useState(
-    task?.state === TaskState.Creating,
+    task?.state === TaskState.Creating
   );
+  const [taskTitle, setTaskTitle] = React.useState("");
 
   function handleEditTask() {
     setIsEditing(true);
+  }
+
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value || "");
   }
 
   function handleExitEditTask() {
     setIsEditing(false);
   }
 
+  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log({ id: task.id, title: taskTitle });
+    // chamada para a função de atualizar;
+    setIsEditing(false);
+  }
+
   return (
-    <Card size='md' className='flex items-center gap-4'>
+    <Card size="md">
       {!isEditing ? (
-        <>
+        <div className="flex items-center gap-4">
           <InputCheckbox
             value={task.concluded?.toString()}
             checked={task.concluded}
           />
-          <Text className={cx('flex-1', { 'line-through': task?.concluded })}>
+          <Text className={cx("flex-1", { "line-through": task?.concluded })}>
             {task?.title}
           </Text>
-          <div className='flex gap-1'>
-            <ButtonIcon icon={TrashIcon} variant='tertiary' />
+          <div className="flex gap-1">
+            <ButtonIcon icon={TrashIcon} variant="tertiary" />
             <ButtonIcon
               icon={PencilIcon}
-              variant='tertiary'
+              variant="tertiary"
               onClick={handleEditTask}
             />
           </div>
-        </>
+        </div>
       ) : (
-        <>
-          <InputText className='flex-1' />
-          <div className='flex gap-1'>
+        <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+          <InputText
+            className="flex-1"
+            onChange={handleChangeTaskTitle}
+            required
+            autoFocus
+          />
+          <div className="flex gap-1">
             <ButtonIcon
               icon={XIcon}
-              variant='secondary'
+              variant="secondary"
+              type="button"
               onClick={handleExitEditTask}
             />
-            <ButtonIcon icon={CheckIcon} variant='primary' />
+            <ButtonIcon type="submit" icon={CheckIcon} variant="primary" />
           </div>
-        </>
+        </form>
       )}
     </Card>
   );
